c1a763e91d84281c7dcb62dfdd84e19e645829dd
F14NDJ1E <firmanuciha@gmail.com>
2022-04-05 12:48:07 +0800

    setup project

---
+++ .history/database/seeders/RolesSeeder_20220404103751.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'name' => 'Admin',
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'User Content',
+        ]);
+    }
+}
