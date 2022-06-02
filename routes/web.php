01e1783fd83ff6984b40fde50fbd7248237e5d6d
Nazlah <afnazlah89@gmail.com>
2022-06-02 23:11:24 +0700

    ganti dashboard link

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin/home');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
